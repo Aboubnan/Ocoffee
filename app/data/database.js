76d0db035795827bc533434a9c3e875a2934ef37
Aboubnan <a.boubnan@outlook.com>
2025-07-04 10:28:39 +0200

    fix: ajout de la config ssl pour psql sur render

--- app/data/database.js
+++ app/data/database.js
@@ -1,10 +1,24 @@
+// import pg from 'pg';
+
+// // 2. Créer un client
+// export const client = new pg.Client(process.env.PG_URL);
+
+// // 3. Connecter le client
+// await client.connect();
+
+// // 4. Exporter le client connecté
+// export default client;
+
 import pg from 'pg';
 
-// 2. Créer un client
-export const client = new pg.Client(process.env.PG_URL);
+const client = new pg.Client({
+  connectionString: process.env.PG_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-// 3. Connecter le client
 await client.connect();
 
-// 4. Exporter le client connecté
 export default client;
+
